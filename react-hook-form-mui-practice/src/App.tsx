0288b0b41d53f5ae38271bdcecaa7018e0176b41
sohye <sohye@squarelab.co>
2020-04-12 23:15:09 +0900

    Add Basic form

--- react-hook-form-mui-practice/src/App.tsx
+++ react-hook-form-mui-practice/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Auth from 'pages/Auth';
+import Basic from 'pages/Basic';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Auth />
+      <Basic />
     </div>
   );
 }
--- react-hook-form-mui-practice/src/pages/Basic.tsx
+++ react-hook-form-mui-practice/src/pages/Basic.tsx
@@ -1,9 +1,94 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import {
+  TextField,
+  Button,
+  MenuItem,
+  makeStyles,
+} from '@material-ui/core';
 
-const Basic: React.FC = () => (
-  <div>
-    Basic Example
-  </div>
-);
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    '& input, & select': {
+      width: 300,
+    },
+  },
+  button: {
+    width: 300,
+    marginTop: 20,
+  },
+});
+
+const companies = [
+  {
+    value: 'squarelab',
+    label: 'Squarelab',
+  },
+  {
+    value: 'kakao',
+    label: 'Kakao',
+  },
+  {
+    value: 'estar',
+    label: 'Estar',
+  },
+  {
+    value: 'airseoul',
+    label: 'Airseoul',
+  },
+];
+
+
+const Basic: React.FC = () => {
+  const { handleSubmit } = useForm();
+  const classes = useStyles();
+  const [company, setCompany] = React.useState('squarelab');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCompany(event.target.value);
+  };
+
+  const onSubmit = (values:any) => {
+    console.log(values);
+  };
+
+  return (
+    <form className={classes.root} onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        name="username"
+        label="username"
+        type="name"
+        required
+      />
+
+      <TextField
+        name="password"
+        label="password"
+        type="password"
+        required
+      />
+
+      <TextField
+        name="company"
+        label="company"
+        select
+        value={company}
+        onChange={handleChange}
+        required
+      >
+        {companies.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
+
+      <Button className={classes.button} type="submit" color="primary">Submit</Button>
+    </form>
+  );
+};
 
 export default Basic;
